6ffa0d78c1b716444e7788dd5acedf05cff23791
kazuha4-sys <149014053+kazuha4-sys@users.noreply.github.com>
2024-04-02 01:21:14 -0300

    Update pulseira.py

--- pulseira.py
+++ pulseira.py
@@ -1,18 +1,19 @@
 import socket
 
 # Configurações do cliente
-host = '127.0.0.1'  # Endereço IP do servidor
-port = 12345  # Porta usada pelo servidor
+host = 'endereco_ip_servidor'  # Endereço IP do servidor
+porta = 12345  # Porta usada pelo servidor
 
 # Criação do socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Conecta ao servidor
-client_socket.connect((host, port))
+client_socket.connect((host, porta))
 
 # Envia uma mensagem para o servidor
-message = "Olá, servidor!"
-client_socket.send(message.encode())
+mensagem = "Olá, servidor!"
+client_socket.send(mensagem.encode())
 
 # Fecha a conexão com o servidor
 client_socket.close()
+
